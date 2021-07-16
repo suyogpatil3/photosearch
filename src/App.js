109dea068db47c4bb95b0ea84129c712a5393694
Suyog Patil <suyog3198@gmail.com>
2021-07-16 14:49:03 +0530

    Changed getRecent default url to dogs images default url

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ function App() {
   }
 
 //for getting default photos
-  const defaultUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=c603ee6ad346f3682d963bc43161ad94&page=${page}&format=json&nojsoncallback=1`
+//changed the default url given by company to a url return default dog images because the provided default url contained adult images
+  // const defaultUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=c603ee6ad346f3682d963bc43161ad94&page=${page}&format=json&nojsoncallback=1`
+  const defaultUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=c603ee6ad346f3682d963bc43161ad94&page=${page}&tags=dogs&format=json&nojsoncallback=1`
   useEffect(() => {
     if(inputQuery==="")
     fetchDataFromUrl(defaultUrl);
